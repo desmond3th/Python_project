b00be15db49920dfa121936dfc9def38a0571332
Saurabh Kumar <106867366+desmond3th@users.noreply.github.com>
2023-03-30 18:16:38 +0530

    Update TSP.py

--- TSP.py
+++ TSP.py
@@ -1,59 +1,80 @@
-# Python3 program to implement traveling salesman 
-# problem using naive approach. 
-#from point import point
-from sys import maxsize 
+from sys import maxsize
 from itertools import permutations
-import matplotlib.pyplot as pt
-#cities
-Ludhiana=(0,61,140,106,93)
-Jalandhar=(61,0,80,149,154)
-Amritsar=(140,80,0,229,235)
-Chandigar=(106,149,229,0,75)
-Patiala=(93,154,235,75,0)
-
+from tkinter import *
 V = 5
- 
+minPath=()
+def travellingSalesmanProblem(graph, s):
+    global minPath;
+    vertex = []
+    for i in range(V):
+        if i != s:
+            vertex.append(i)
 
-# implementation of traveling Salesman Problem 
-def travellingSalesmanProblem(graph, s): 
- 
-    # store all vertex apart from source vertex 
-    vertex = [] 
-    for i in range(V): 
-        if i != s: 
-            vertex.append(i) 
- 
-    # store minimum weight Hamiltonian Cycle 
-    min_path = maxsize 
+    min_path = maxsize
     next_permutation=permutations(vertex)
     for i in next_permutation:
- 
-        # store current Path weight(cost) 
         current_pathweight = 0
- 
-        # compute current path weight 
-        k = s 
-        for j in i: 
-            current_pathweight += graph[k][j] 
-            k = j 
-        current_pathweight += graph[k][s] 
- 
-        # update minimum 
-        min_path = min(min_path, current_pathweight) 
-         
-    return min_path 
+        k = s
+        tempMinPath = i
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+        if current_pathweight<min_path:
+            minPath=tempMinPath
+        min_path = min(min_path, current_pathweight)    
+    return min_path
 
-print('Shortest path between cities: ')
- 
- 
-if __name__ == "__main__": 
- 
-    # __matrix representation of graph__
-    graph = [[0, 61, 140, 106,93], [61,0,80,149,154], 
-            [140,80,0,229,235], [106, 149, 229, 0,75],[93,154,235,75,0]] 
-    s = 0
-    
-    print(travellingSalesmanProblem(graph, s))
- ## Graph implementation using matplotlib
-    pt.plot([0,61,140,106,93],[61,0,80,149,154],[140,800,229,0,75],[93,154,235,75,0],)
-    pt.title('Implementation of traveling Salesman Problem')
+
+
+print("Enter the names of all the cities that you want to travel");
+a=input("Enter the name of place one : ");
+b=input("Enter the name of place two : ");
+c=input("Enter the name of place three : ")
+d=input("Enter the name of place four : ");
+e=input("Enter the name of place five : ")
+
+ab=int(input(f"Enter the distance between {a} and {b} : "));
+ac=int(input(f"Enter the distance between {a} and {c} : "));
+ad=int(input(f"Enter the distance between {a} and {d} : "));
+ae=int(input(f"Enter the distance between {a} and {e} : "));
+bc=int(input(f"Enter the distance between {b} and {c} : "));
+bd=int(input(f"Enter the distance between {b} and {d} : "));
+be=int(input(f"Enter the distance between {b} and {e} : "));
+cd=int(input(f"Enter the distance between {c} and {d} : "));
+ce=int(input(f"Enter the distance between {c} and {e} : "));
+de=int(input(f"Enter the distance between {d} and {e} : "));
+
+
+graph=[[0,ab ,ac ,ad , ae] , [ab , 0 , bc , bd , be],[ac , bc , 0,cd , ce] , [ad , bd , cd , 0 , de],[ae, be, ce , de,0]];
+
+
+print(graph)
+
+print(f"0.{a}")
+print(f"1.{b}")
+print(f"2.{c}")
+print(f"3.{d}")
+print(f"4.{e}")
+answer='YES'
+while(answer=='YES' or answer=='yes'):
+    s=int(input("Select from given options (enter a number) that from where you want to start your journey : "));
+    answer=travellingSalesmanProblem(graph, s);
+    answer=str(answer)+' KM'
+    resulter="The distance of the shortest path is : "+ answer;
+    cities=[a,b,c,d,e]
+    window=Tk()
+    window.geometry('500x500')
+    Label(text=resulter,font = "Verdana 10 bold",fg = "white", bg = "black").pack()
+    Label(text=cities).pack()
+    Label(text="The Path is : ",font = "Verdana 15 bold").pack()
+    Label(text=cities[s],font = "Verdana 10 bold",fg = "white", bg = "black").pack()
+    Label(text='↓',font = "Verdana 10 bold").pack()
+    for x in minPath:
+        Label(text=cities[x],font = "Verdana 10 bold",fg = "white", bg = "black",borderwidth=5).pack()
+        Label(text='↓',font = "Verdana 10 bold").pack()
+
+    Label(text=cities[s],font = "Verdana 10 bold",fg = "white", bg = "black").pack()
+    B1=Button (window,text='Do YOU WANT TO CHANGE YOUR LOCATION',command=window.destroy,borderwidth=5).pack()
+    window.mainloop()
+    answer=input("DO you want to change the starting point(YES/NO)?")
